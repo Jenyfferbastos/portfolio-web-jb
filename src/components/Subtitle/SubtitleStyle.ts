f45b5de02524c44e891656fa6ca364eba970e74e
Jenyffer Bastos <jenyfferbastos06@gmail.com>
2023-09-23 14:30:37 -0300

    changing style settings

--- src/components/Subtitle/SubtitleStyle.ts
+++ src/components/Subtitle/SubtitleStyle.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const SubtitleStyle = styled.h3`
   font-size: 16px;
   font-weight: 500;
-  margin-top: 10px;
+  margin: 10px 50px 0px 50px;
+  text-align: center;
 
   @media only screen and (max-width: 786px) {
     font-size: 16px;
