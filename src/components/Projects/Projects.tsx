95f01548b03ad3ab2fb6a9f9ba3fb250471881b4
Jenyffer Bastos <jenyfferbastos06@gmail.com>
2023-09-23 11:42:03 -0300

    creating project page

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import { ProjectsStyle } from "./ProjectsStyle";
+import { Card } from "../Card/Card";
+import { Title } from "../Title/Title";
+
+export function Projects() {
+
+  return (
+    <ProjectsStyle>
+      <Title title={"Projetos"}></Title>
+      <Card />
+    </ProjectsStyle>
+  );
+}
---
+++ src/components/Projects/ProjectsStyle.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ProjectsStyle = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  gap: 2rem;
+  width: 80rem;
+  margin-top: 12%;
+  
+
+  @media only screen and (max-width: 990px) {
+    margin-top: 45%;
+  }
+  @media only screen and (max-width: 768px) {
+    margin-top: 27rem;
+  }
+`;
+
