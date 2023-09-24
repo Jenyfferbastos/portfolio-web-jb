465addf4572a076f11ec23648eaadd48a3711527
Jenyffer Bastos <jenyfferbastos06@gmail.com>
2023-09-24 11:22:24 -0300

    hosting cv link on drive

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,7 +11,7 @@ export function Home() {
       <h1>Engenheira de Software</h1>
       <Text>Fico feliz em vê-lo (a) por aqui.</Text>
       <ButtonContainer>
-        <Button text="Currículo" color={"#EA6AA9"} icon={FaRegFilePdf} children={undefined}  downloadUrl="../../assets/documento/Jenyffer Bastos Sacramento_CV.docx"/>
+        <Button text="Currículo" color={"#EA6AA9"} icon={FaRegFilePdf} children={undefined}  link="https://drive.google.com/file/d/13PF49ksa5wkVjYBS4PPzXKa7EoDEQIRX/view?usp=sharing" isExternal={true} />
         <Button text="GitHub" color={"transparent"} icon={VscGithubAlt} children={undefined} link="https://github.com/Jenyfferbastos" isExternal={true}></Button>
       </ButtonContainer>
     </HomeStyle>
