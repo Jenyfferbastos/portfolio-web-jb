2602e5d434f1b6acea458b2f670a0e77792fd846
Jenyffer Bastos <jenyfferbastos06@gmail.com>
2023-09-24 11:40:28 -0300

    adding links to the footer section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,73 @@
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
-import { ContactStyle, ContainerFooterSections, CopyrightStyle, FooterSection, FooterStyle, IconContainer, ListItem, Title } from "./FooterStyle";
+import {
+  ContactStyle,
+  ContainerFooterSections,
+  CopyrightStyle,
+  FooterSection,
+  FooterStyle,
+  IconContainer,
+  ListItem,
+  Title,
+} from "./FooterStyle";
 
-export function Footer(){
-  return(
+const footerLinks = [
+  { path: "about", text: "Sobre mim" },
+  { path: "technical-skills", text: "Habilidades Técnicas" },
+  { path: "projects", text: "Projetos" },
+  { path: "education", text: "Educação" },
+  { path: "services", text: "Serviços" },
+  { path: "contact", text: "Contato" },
+];
+
+const socialLinks = [
+  { text: "LinkedIn", url: "https://www.linkedin.com/in/jenyffer-bastos-41098b1b7/" },
+  { text: "Instagram", url: "https://www.instagram.com/jenyffrbastos/" },
+  { text: "Twitter", url: "https://twitter.com/JenyfferBastosS" },
+];
+
+export function Footer() {
+  return (
     <FooterStyle>
-       <ContainerFooterSections>
-            <FooterSection>
-        <Title>MENU</Title>
-        <ul>
-          <ListItem>Sobre mim</ListItem>
-          <ListItem>Projetos</ListItem>
-          <ListItem>Habilidades Técnicas</ListItem>
-          <ListItem>Educação</ListItem>
-          <ListItem>Serviços</ListItem>
-          <ListItem>Contato</ListItem>
-        </ul>
-      </FooterSection>
-      <FooterSection>
-        <Title>REDES SOCIAIS</Title>
-        <ul>
-          <ListItem>LinkedIn</ListItem>
-          <ListItem>Instagram</ListItem>
-          <ListItem>Twitter</ListItem>
-        </ul>
-      </FooterSection>
-      <FooterSection>
-        <ContactStyle ><IconContainer>< BsFillTelephoneFill size={18}/></IconContainer>+55 75 99852-2555</ContactStyle>
-        <ContactStyle><IconContainer>< MdEmail size={18} /></IconContainer>jenyfferbastos06@gmail.com</ContactStyle>
-      </FooterSection>
-     </ContainerFooterSections>
+      <ContainerFooterSections>
+        <FooterSection>
+          <Title>MENU</Title>
+          <ul>
+            {footerLinks.map((link, index) => (
+              <ListItem key={index}>
+                <a href={`#${link.path}`}>{link.text}</a>
+              </ListItem>
+            ))}
+          </ul>
+        </FooterSection>
+        <FooterSection>
+          <Title>REDES SOCIAIS</Title>
+          <ul>
+            {socialLinks.map((link, index) => (
+              <ListItem key={index}>
+                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                  {link.text}
+                </a>
+              </ListItem>
+            ))}
+          </ul>
+        </FooterSection>
+        <FooterSection>
+          <ContactStyle>
+            <IconContainer>
+              <BsFillTelephoneFill size={18} />
+            </IconContainer>
+            +55 75 99852-2555
+          </ContactStyle>
+          <ContactStyle>
+            <IconContainer>
+              <MdEmail size={18} />
+            </IconContainer>
+            jenyfferbastos06@gmail.com
+          </ContactStyle>
+        </FooterSection>
+      </ContainerFooterSections>
       <CopyrightStyle>&copy; 2023 Jenyffer Bastos</CopyrightStyle>
     </FooterStyle>
-  )
+  );
 }
