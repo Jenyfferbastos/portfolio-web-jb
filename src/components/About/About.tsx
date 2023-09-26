dc57314a85a1338251bdab96cfc7714d73e98db5
Jenyffer Bastos <jenyfferbastos06@gmail.com>
2023-09-26 15:18:11 -0300

    style correction

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -26,10 +26,10 @@ export function About() {
            " E eu amo o que faço.
         </MainText>
         <Description>
-        Desenvolvedora front-end e Instrutora de Tecnologia, meu percurso é marcado por um Bacharelado em Engenharia de Software e a conclusão de +30 cursos na área. 
+        Desenvolvedora front-end e Instrutora de Tecnologia, meu percurso é marcado por um Bacharelado em andamento em Engenharia de Software e a conclusão de +30 cursos na área. 
         </Description>
         <Description>
-        Com experiências notáveis em projetos voluntários em ONGs, mentoria de iniciantes pela WoMakersCode e contribuições para projetos Open Source, busco constantemente impactar através do código. Minha jornada é moldada pelo compromisso em superar limites, sempre com o objetivo de fazer a diferença no mundo da tecnologia.
+        Com experiências notáveis em projetos voluntários em ONGs, como no Instituto Busidô e Rede Impulsoma,  busco constantemente impactar através do código. Minha jornada é moldada pelo compromisso em superar limites, sempre com o objetivo de fazer a diferença no mundo da tecnologia.
         </Description>
         <ButtonWrapper>
           <Button color="#EA6AA9" text="LinkedIn" icon={BsLinkedin} children={undefined} link="https://www.linkedin.com/in/jenyffer-bastos-41098b1b7/"  isExternal={true}/>
